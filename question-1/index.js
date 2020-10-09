3f1a111ff340db941772b889186e14486e8fa1d8
Rafael Silva <rafonsosilva@gmail.com>
2020-10-08 21:00:15 -0400

    Fixed question 1

--- question-1/index.js
+++ question-1/index.js
@@ -1,43 +1,31 @@
-const mixedArray = ['PIZZA', 10, true, 25, false, 'Wings']
+const mixedArray = ["PIZZA", 10, true, 25, false, "Wings"];
 
 const lowerCaseWords = (array) => {
-    const checkNotString = (name) => {
-      return typeof name == "string";
-    };
-  
-    return new Promise(function (resolve, reject) {
-      if (array.some(checkNotString)) {
-        const lowerCaseArray = array.map((name) => {
-            if(typeof name == "string"){
-                return name.toLowerCase();
-            }
-        });
-            
-        resolve(lowerCaseArray);
-      } else {
-        reject(new Error("There are no words to return."));
-      }
-    });
+  const checkNotString = (name) => {
+    return typeof name == "string";
   };
 
-  const removeUndefined = (lowerCaseArray) => {
-      return new Promise(function (resolve){
-          const onlyWords = lowerCaseArray => {
-            let newArray = []  
-            for (let index = 0; index < lowerCaseArray.length; index++) {
-                if(typeof array[index] != "undefined"){
-                    newArray.push(array[index]);
-                }
-            }
-            return newArray;
-          }
-          resolve(onlyWords);
-      })
-  }
-  
-  lowerCaseWords(mixedArray)
-    .then(removeUndefined)
-    .then((result) => console.log(result))
-    //.catch((error) => console.log(error));
-  
-  
+  return new Promise(function (resolve, reject) {
+    if (array.some(checkNotString)) {
+      const getOnlyWords = array.filter(
+        (element) => typeof element == "string"
+      );
+      
+      resolve(getOnlyWords);
+    } else {
+      reject(new Error("There are no words to return."));
+    }
+  });
+};
+
+const lowerCaseArray = (lowerCaseWords) => {
+  return new Promise(function (resolve) {
+    const lcWords = lowerCaseWords.map((word) => word.toLowerCase());
+    resolve(lcWords)
+  })
+};
+
+lowerCaseWords(mixedArray)
+  .then(lowerCaseArray)
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
